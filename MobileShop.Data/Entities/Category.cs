d9a94bc708002ad793f443121fa6fdc8623f7455
PHUNG-199676\PHUNG-199676 <quocphungcn80@gmail.com>
2022-12-05 20:22:30 +0700

    upcode database

---
+++ MobileShop.Data/Entities/Category.cs
@@ -0,0 +1,13 @@
+﻿using MobileShop.Data.Enum;
+
+namespace MobileShop.Data.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public int SordOrder { get; set; }
+        public bool IsShowHome { get; set; }
+        public int? ParentId { get; set; }
+        public Status Status { get; set; }
+    }
+}
---
+++ MobileShop.Data/Entities/Transaction.cs
@@ -0,0 +1,19 @@
+﻿using MobileShop.Data.Enum;
+using System;
+
+namespace MobileShop.Data.Entities
+{
+    public class Transaction
+    {
+        public int Id { set; get; }
+        public DateTime TransactionDate { set; get; }
+        public string ExternalTransactionId { set; get; }
+        public decimal Amount { set; get; }
+        public decimal Fee { set; get; }
+        public string Result { set; get; }
+        public string Message { set; get; }
+        public TransactionStatus Status { set; get; }
+        public string Provider { set; get; }
+
+    }
+}
