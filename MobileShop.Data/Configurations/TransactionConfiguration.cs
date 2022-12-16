a851c5fe10d7b8e5d4825fa79daefac21fe291c9
PHUNG-199676\PHUNG-199676 <quocphungcn80@gmail.com>
2022-12-16 14:18:02 +0700

    upcode

--- MobileShop.Data/Configurations/TransactionConfiguration.cs
+++ MobileShop.Data/Configurations/TransactionConfiguration.cs
@@ -10,7 +10,8 @@ namespace MobileShop.Data.Configurations
         {
             builder.ToTable("Transactions");
             builder.HasKey(x => x.Id);
-            builder.Property(x => x.Id).UseIdentityColumn();
+            builder.Property(x => x.Id).UseIdentityColumn(); 
+            builder.HasOne(x => x.AppUser).WithMany(x => x.Transactions).HasForeignKey(x => x.UserId);
         }
     }
 }
