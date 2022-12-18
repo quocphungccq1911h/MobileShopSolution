2c01bc3143d5273cc52aed71c3276279f6c82dd1
PHUNG-199676\PHUNG-199676 <quocphungcn80@gmail.com>
2022-12-18 10:32:14 +0700

    commit

--- MobileShop.WebAPI/Startup.cs
+++ MobileShop.WebAPI/Startup.cs
@@ -1,19 +1,12 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using MobileShop.Application.Catalog.Product;
 using MobileShop.Data;
 using MobileShop.Utilities.Constants;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace MobileShop.WebAPI
 {
