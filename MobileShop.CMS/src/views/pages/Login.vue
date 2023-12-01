209013fa1c5eb0c3c547c0a131027c0b48726297
PHUNG-PC <phung-pc>
2023-12-01 22:08:09 +0700

    upcode

--- MobileShop.CMS/src/views/pages/Login.vue
+++ MobileShop.CMS/src/views/pages/Login.vue
@@ -1,6 +1,58 @@
 <template>
-    <div>
-        <h1>Page Login</h1>
+    <div class="bg-light min-vh-100 d-flex flex-row align-items-center">
+        <CContainer>
+            <CRow class="justify-content-center">
+                <CCol :md="8">
+                    <CCardGroup>
+                        <CCard class="p-4">
+                            <CCardBody>
+                                <CForm>
+                                    <h1>Đăng nhập</h1>
+                                    <p class="text-medium-emphasis">Đăng nhập vào tài khoản của bạn
+                                    </p>
+                                    <CInputGroup class="mb-3">
+                                        <CInputGroupText>
+                                            <CIcon icon="cil-user" />
+                                        </CInputGroupText>
+                                        <CFormInput placeholder="Tài khoản" autocomplete="username" />
+                                    </CInputGroup>
+                                    <CInputGroup class="mb-4">
+                                        <CInputGroupText>
+                                            <CIcon icon="cil-lock-locked" />
+                                        </CInputGroupText>
+                                        <CFormInput type="password" placeholder="Mật khẩu"
+                                            autocomplete="current-password" />
+                                    </CInputGroup>
+                                    <CRow>
+                                        <CCol :xs="6">
+                                            <CButton color="primary" class="px-4"> Đăng nhập </CButton>
+                                        </CCol>
+                                        <CCol :xs="6" class="text-right">
+                                            <CButton color="link" class="px-0">
+                                                Quên mật khẩu?
+                                            </CButton>
+                                        </CCol>
+                                    </CRow>
+                                </CForm>
+                            </CCardBody>
+                        </CCard>
+                        <CCard class="text-white bg-primary py-5" style="width: 44%">
+                            <CCardBody class="text-center">
+                                <div>
+                                    <h2>Đăng ký</h2>
+                                    <p>
+                                        Nếu bạn chưa có tài khoản. Hãy đăng ký một tài khoản!
+                                    </p>
+                                    <CButton color="light" variant="outline" class="mt-3">
+                                        Đăng ký ngay!
+                                    </CButton>
+                                </div>
+                            </CCardBody>
+                        </CCard>
+                    </CCardGroup>
+                </CCol>
+            </CRow>
+        </CContainer>
     </div>
 </template>
 <script>
--- MobileShop.CMS/src/views/pages/Register.vue
+++ MobileShop.CMS/src/views/pages/Register.vue
@@ -1,8 +1,54 @@
-<template lang="">
-    <div>
-        <h1>Page Register</h1>
+<template>
+    <div class="bg-light min-vh-100 d-flex flex-row align-items-center">
+      <CContainer>
+        <CRow class="justify-content-center">
+          <CCol :md="9" :lg="7" :xl="6">
+            <CCard class="mx-4">
+              <CCardBody class="p-4">
+                <CForm>
+                  <h1>Đăng  ký</h1>
+                  <p class="text-medium-emphasis">Tạo một tài khoản</p>
+                  <CInputGroup class="mb-3">
+                    <CInputGroupText>
+                      <CIcon icon="cil-user" />
+                    </CInputGroupText>
+                    <CFormInput placeholder="Tên đăng nhập" autocomplete="username" />
+                  </CInputGroup>
+                  <CInputGroup class="mb-3">
+                    <CInputGroupText>@</CInputGroupText>
+                    <CFormInput placeholder="Email" autocomplete="email" />
+                  </CInputGroup>
+                  <CInputGroup class="mb-3">
+                    <CInputGroupText>
+                      <CIcon icon="cil-lock-locked" />
+                    </CInputGroupText>
+                    <CFormInput
+                      type="password"
+                      placeholder="Mật khẩu"
+                      autocomplete="new-password"
+                    />
+                  </CInputGroup>
+                  <CInputGroup class="mb-4">
+                    <CInputGroupText>
+                      <CIcon icon="cil-lock-locked" />
+                    </CInputGroupText>
+                    <CFormInput
+                      type="password"
+                      placeholder="Xác nhận mật khẩu"
+                      autocomplete="new-password"
+                    />
+                  </CInputGroup>
+                  <div class="d-grid">
+                    <CButton color="success">Đăng ký</CButton>
+                  </div>
+                </CForm>
+              </CCardBody>
+            </CCard>
+          </CCol>
+        </CRow>
+      </CContainer>
     </div>
-</template>
+  </template>
 <script>
 export default {
     name: 'Register',
