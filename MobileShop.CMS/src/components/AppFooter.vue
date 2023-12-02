c037e23cd768007177b512d6ebc1bb104e94f7fa
PHUNG-PC <phung-pc>
2023-12-02 21:31:31 +0700

    commit dashboard

--- MobileShop.CMS/src/components/AppFooter.vue
+++ MobileShop.CMS/src/components/AppFooter.vue
@@ -1,7 +1,14 @@
 <template>
-    <div>
-        <h1>Footer</h1>
-    </div>
+    <CFooter>
+        <div>
+            <a href="https://cms-mobileshop.vercel.app/" target="_blank">CMS.MobileShop</a>
+            <span class="ms-1">&copy; {{ new Date().getFullYear() }} Demo </span>
+        </div>
+        <div class="ms-auto">
+            <span class="me-1" target="_blank">Powered by</span>
+            <a href="https://coreui.io/vue">CoreUI</a>
+        </div>
+    </CFooter>
 </template>
 
 <script>
